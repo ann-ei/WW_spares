e01b3db44d9bfff41c2aada0a8e9b9727ab9514f
Anna Tykhoneko <annsed001@gmail.com>
2022-01-14 01:21:40 +0200

    верстка

---
+++ JS/slider-service.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(".slider-service").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    speed: 500,
+    mobileFirst: true,
+
+    responsive: [
+      {
+        breakpoint: 300,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
+});
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# WW_spares
- 
+# WV_spares
+
+Список страниц | Исполнитель
+
+---
+
+Главная - index.html (\_first-header.scss, \_hero.scss, \_service-maintenance.scss, \_cost-of-work.scss, \_sale-of-spare-parts.scss, \_popular-categories.scss, \_parts-in-stock.scss, \_free-consultation.scss, ) |
+Услуга - service.html (\_second-header.scss) |
+Оформление заказа - ordering.html |
+Новости - news-list.html |
+Новость - news-item.html |
+Корзина - shopping-cart.html |
+Контакты - contacts.html |
+Категория - category-item.html |
+Каталог - catalog.html |
+Карточка товара - card-product.html |
+Заказ принят - order-accepted.html |
